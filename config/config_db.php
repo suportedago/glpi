c1cb9b1a94e46fe2a05ccb9ccde990a17539524e
Charlan <charlanbettiol@gmail.com>
2016-06-21 11:41:53 -0300

    teste

---
+++ config/config_db.php
@@ -0,0 +1,13 @@
+<?php
+ class DB extends DBmysql {
+                
+ var $dbhost = 'localhost:3306';
+                
+ var $dbuser 	= 'root';
+                
+ var $dbpassword= '';
+                
+ var $dbdefault	= 'glpi';
+                
+ } 
+?>
